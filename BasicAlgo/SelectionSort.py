a88966c655df168c52188cd0fb9f1565577e1648
Amin Abdollahpour <mohammadaminabdolahpour@gamil.com>
2023-07-16 23:44:22 +0430

    selection sort

---
+++ BasicAlgo/SelectionSort.py
@@ -0,0 +1,10 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+
+    return arr
+
+
+
