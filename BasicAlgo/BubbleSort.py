ac5df296dcace800827a818bcdb15df9909c1cd9
Amin Abdollahpour <mohammadaminabdolahpour@gamil.com>
2023-07-19 04:06:55 +0430

    bubble added

---
+++ BasicAlgo/BubbleSort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+
+
