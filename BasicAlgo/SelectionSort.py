44e595d92c837b2421511f5da157d70e193ab45f
Amin Abdollahpour <mohammadaminabdolahpour@gamil.com>
2023-07-19 03:06:41 +0430

    selection sort edited

--- BasicAlgo/SelectionSort.py
+++ BasicAlgo/SelectionSort.py
@@ -1,10 +1,9 @@
 def selection_sort(arr):
     for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+        min_idx = i
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i] , arr[min_idx] = arr[min_idx] , arr[i]
 
     return arr
-
-
-
