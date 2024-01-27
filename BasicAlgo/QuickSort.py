012cebf3907e71817fe71b8f8a4de54a2d6fbb8c
asus <mohammadaminabdolahpour@gmail.com>
2024-01-27 19:46:49 +0330

    quick sort

---
+++ BasicAlgo/QuickSort.py
@@ -0,0 +1,10 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return quick_sort(left) + [pivot] + quick_sort(right)
+
+
